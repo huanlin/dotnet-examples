31d4d490682d02e34b2a7570b65c1be9cfc3d318
Michael Tsai <huanlin.tsai@gmail.com>
2015-04-18 23:49:04 +0800

    Added TraceWriterDemo.

---
+++ DotNet/Web/WebAPI/TraceWriterDemo/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TraceWriterDemo.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        public string Get()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
