11a847d65423e4c21a1327d4ed9b2d9cf9777a27
Michael Tsai <michael.tsai@afusion.com>
2015-06-05 15:56:47 +0800

    Added FluentAssertionsDemo.

---
+++ DotNet/Testing/Unit Testing/FluentAssertionsDemo/MyLib/MyClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class MyClass
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public string GetHelloString(string name)
+        {
+            return "Hello, " + name;
+        }
+    }
+}
---
+++ DotNet/Testing/Unit Testing/FluentAssertionsDemo/MyLibTests/MyClassTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MyLib;
+using NUnit.Framework;
+
+namespace MyLibTests
+{
+    [TestFixture]
+    public class MyClassTests
+    {
+        [Test]
+        public void CanAddTwoNumbers()
+        {
+            var sut = new MyClass();
+            int result = sut.Add(10, 20);
+
+            Assert.That(result, Is.EqualTo(30));
+        }
+
+        [Test]
+        public void CanAddTwoNumbers_Fluent()
+        {
+            var sut = new MyClass();
+            int result = sut.Add(1, 1);
+            result.Should().Be(2, "because 1+1=2.");
+        }
+
+        [Test]
+        public void CanGetHelloString_Fluent()
+        {
+            var sut = new MyClass();
+            string result = sut.GetHelloString("Michael");
+
+            result.Should().StartWith("Hello").And.EndWith("Michael");
+        }
+
+    }
+}
